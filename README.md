7bf7b69b206937febd78782797e6642ca33df497
Tal Malka <tal.malka1@cyberark.com>
2024-05-29 19:57:42 +0300

    0.1

---
+++ README.md
@@ -0,0 +1,14 @@
+# PSMPChecker
+
+## Description
+
+This Python tool provides functionality to check compatibility between the installed version of a proprietary software (PSMP) and various Linux distributions. It retrieves the installed PSMP version and the Linux distribution version, then checks if the combination is supported based on a predefined set of compatibility rules.
+
+## Installation
+
+To install the dependencies required for this tool, you can use pip and the provided `requirements.txt` file. Make sure you have Python and pip installed on your system. Then, follow these steps:
+
+```bash
+git clone https://github.com/yourusername/your-project.git
+cd your-project
+pip install -r requirements.txt
--- main.py
+++ main.py
@@ -2,52 +2,61 @@ import json
 import subprocess
 import distro
 
-def load_psmp_versions_from_json(file_path):
+def load_psmp_versions_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
 def get_installed_psmp_version():
     try:
-        # result = subprocess.check_output("rpm -qa | grep -i cark", shell=True, universal_newlines=True).strip()
-        result = "CARKpsmp-14.0.0-14.x86_64"
+        result = subprocess.check_output("rpm -qa | grep -i cark", shell=True, universal_newlines=True).strip()
         if result:
-            # Extract version number, assuming the result format is "CARKpsmp-version"
-            version = result.split('-')[1]  # Take the part after "CARKpsmp-"
-            version = ".".join(version.split('.')[:2])  # Take only major and minor version
-            return version
+            # Extract version number, assuming the result format is "CARKpsmp-14.0.0-14.x86_64"
+            version = result.split('-')[1]
+            # Extract major and minor version numbers
+            major, minor, _ = version.split('.', 2)
+            main_version = f"{major}.{minor}"
+            return main_version
     except subprocess.CalledProcessError:
         return None
 
 def get_linux_distribution():
-    return distro.name(), distro.version()
+    version_info = distro.version(best=True)
+    major, minor, _ = version_info.split('.', 2)
+    main_version = f"{major}.{minor}"
+    return distro.name(), main_version
 
 def is_supported(psmp_versions, psmp_version, distro_name, distro_version):
     if psmp_version not in psmp_versions:
         return False
-
-    for distro in psmp_versions[psmp_version]['supported_distributions']:
-        distro_name_lower = distro['name'].lower()
-        distro_version_lower = [v.lower() for v in distro['versions']]
-        if distro_name_lower == distro_name.lower() and distro_version.lower() in distro_version_lower:
-            return True
+    for version in psmp_versions:
+        if version.startswith(psmp_version):  # Check if PSMP version starts with given major and minor version
+            for distro_info in psmp_versions[version]['supported_distributions']:
+                if distro_info['name'].lower() == distro_name.lower():
+                    # Check if the distro version matches any of the supported versions
+                    for supported_version in distro_info.get('versions', []):
+                        if distro_version.startswith(supported_version):
+                            return True
     return False
 
+
 # Load PSMP versions from a JSON file
-psmp_versions = load_psmp_versions_from_json('src/versions.json')
+psmp_versions = load_psmp_versions_json('src/versions.json')
 
 # Get the installed PSMP version
 psmp_version = get_installed_psmp_version()
-print(f"Installed PSMP version: {psmp_version}")
 if not psmp_version:
     print("No PSMP version found or PSMP is not installed.")
     exit(1)
 
 # Get the Linux distribution and version
 distro_name, distro_version = get_linux_distribution()
+
+print(f"PSMP version: {psmp_version}")
 print(f"Linux distribution: {distro_name} {distro_version}")
+
 
 # Check compatibility
 if is_supported(psmp_versions, psmp_version, distro_name, distro_version):
-    print(f"PSMP version {psmp_version} supports {distro_name} {distro_version}")
+    print(f"PSMP version {psmp_version} Supports {distro_name} {distro_version}")
 else:
-    print(f"PSMP version {psmp_version} does not support {distro_name} {distro_version}")
+    print(f"PSMP version {psmp_version} Does Not Support {distro_name} {distro_version}")
