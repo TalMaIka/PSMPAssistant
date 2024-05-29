ecbf754ed2286be3c2719638d901ccd5e964c8e3
Tal Malka <tal.coder@gmail.com>
2024-05-29 14:59:27 +0300

    0.1

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ def load_psmp_versions_from_json(file_path):
 
 def get_installed_psmp_version():
     try:
-        result = subprocess.check_output("rpm -qa | grep -I cark", shell=True, universal_newlines=True).strip()
+        # result = subprocess.check_output("rpm -qa | grep -i cark", shell=True, universal_newlines=True).strip()
+        result = "CARKpsmp-14.0.0-14.x86_64"
         if result:
-            # Extract version number, assuming the result format is "cark-version"
-            version = result.split('-')[1]
+            # Extract version number, assuming the result format is "CARKpsmp-version"
+            version = result.split('-')[1]  # Take the part after "CARKpsmp-"
+            version = ".".join(version.split('.')[:2])  # Take only major and minor version
             return version
     except subprocess.CalledProcessError:
         return None
@@ -22,8 +24,11 @@ def get_linux_distribution():
 def is_supported(psmp_versions, psmp_version, distro_name, distro_version):
     if psmp_version not in psmp_versions:
         return False
+
     for distro in psmp_versions[psmp_version]['supported_distributions']:
-        if distro['name'].lower() == distro_name.lower() and distro_version in distro['versions']:
+        distro_name_lower = distro['name'].lower()
+        distro_version_lower = [v.lower() for v in distro['versions']]
+        if distro_name_lower == distro_name.lower() and distro_version.lower() in distro_version_lower:
             return True
     return False
 
@@ -32,12 +37,14 @@ psmp_versions = load_psmp_versions_from_json('src/versions.json')
 
 # Get the installed PSMP version
 psmp_version = get_installed_psmp_version()
+print(f"Installed PSMP version: {psmp_version}")
 if not psmp_version:
     print("No PSMP version found or PSMP is not installed.")
     exit(1)
 
 # Get the Linux distribution and version
 distro_name, distro_version = get_linux_distribution()
+print(f"Linux distribution: {distro_name} {distro_version}")
 
 # Check compatibility
 if is_supported(psmp_versions, psmp_version, distro_name, distro_version):
